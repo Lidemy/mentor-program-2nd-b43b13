d7146efa226a0db36685349892156c7a0e1bc623
ABBIE <ABBIE@MacdeMacBook-Pro-4.local>
2018-09-06 21:01:31 +0800

    hw1-6

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,12 @@
 function reverse(str) {
+	var result=''
+	for (var i = str.length-1; i >= 0; i--) {
+		result += str[i]
+	}
+	console.log(result)
 
 }
+
+reverse('yoyoyo') //oyoyoy
+reverse('1abc2') //2bca1
+reverse('1,2,3,2,1') //1,2,3,2,1
