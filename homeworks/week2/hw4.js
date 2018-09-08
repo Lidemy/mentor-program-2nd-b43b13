2c0420227e9bfbb1796612b44570a54ce968b03f
ABBIE <ABBIE@MacdeMacBook-Pro-4.local>
2018-09-08 17:10:10 +0800

    week2-hw4

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,12 @@
 function isPalindromes(str) {
-  
+	var result = ''
+	for (var i = str.length-1; i >= 0; i--) {
+		result += str[i]
+	}
+  if (result == str) {
+  	return true
+  }
+  return false
 }
 
 module.exports = isPalindromes
