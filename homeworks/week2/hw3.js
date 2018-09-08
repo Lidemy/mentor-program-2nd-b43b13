0bdd264da00776a82c8d0d0a2c093c3f6e418a12
ABBIE <ABBIE@MacdeMacBook-Pro-4.local>
2018-09-08 16:59:50 +0800

    week2-hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,15 @@
 function isPrime(n) {
-  
+	if (n >= 1 && n <= 100000) {
+		if( n == 1) {
+			return false
+		}
+		for (var i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false
+			}
+		}
+		return true
+	}  	
 }
 
 module.exports = isPrime
